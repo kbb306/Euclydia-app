b1385a4329bdf86a072423eadf706b72c19a9724
kbb306 <kylerbbelshaw@gmail.com>
2026-03-11 08:16:35 -0600

    Added DNA.kt

---
+++ app/src/main/java/com/example/euclydia/model/DNA.kt
@@ -0,0 +1,21 @@
+package com.example.euclydia.model
+
+import android.graphics.Color
+import java.util.UUID
+import kotlin.uuid.Uuid
+
+data class DNA (
+    val uuid: UUID,
+    val name: String,
+    val age: Age,
+    val gender: Gender,
+    val color: Int,
+    val sides: Int,
+    val length: Double,
+    val x: Double,
+    val y: Double,
+    val heading: Double,
+    val speed: Double,
+    val lines: MutableList<String>,
+    val canon: SpecialVoice?
+)
