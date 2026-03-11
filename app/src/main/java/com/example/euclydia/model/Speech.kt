f23b53b1901c5f3a34d403f3e8a6aed053b08bd2
kbb306 <kylerbbelshaw@gmail.com>
2026-03-11 04:56:02 -0600

    Created voice map and SC/EU override. Set failsafe voice to Androgynous Adult (protocol 9)

---
+++ app/src/main/java/com/example/euclydia/model/Speech.kt
@@ -0,0 +1,47 @@
+package com.example.euclydia.model
+
+import android.speech.tts.Voice
+import br.com.chatnoir.ggwave_kotlin.GGWaveCodec
+import br.com.chatnoir.ggwave_kotlin.GGWaveSampleFormat
+
+class Speech(
+) {
+    private val codecs = mapOf<Gender,Map<Age, GGWaveCodec>>(
+        Gender.FEMALE to mapOf(
+            Age.CHILD to makeCodec(2),
+            Age.ADULT to makeCodec(1)
+        ),
+        Gender.MALE to mapOf(
+            Age.CHILD to makeCodec(8),
+            Age.ADULT to makeCodec(7)
+        ),
+        Gender.ANDROGYNOUS to mapOf(
+            Age.CHILD to makeCodec(10),
+            Age.ADULT to makeCodec(9)
+        )
+    )
+
+        private fun makeCodec(protocol : Int) : GGWaveCodec {
+        val voice = GGWaveCodec.Builder()
+            .sampleRate(48000f)
+            .sampleFormatInp(GGWaveSampleFormat.I16)
+            .sampleFormatOut(GGWaveSampleFormat.I16)
+            .protocolId(protocol)
+            .volume(5)
+            .build()
+        return voice
+    }
+    fun codecFor(age: Age, gender: Gender, specialVoice: SpecialVoice? = null): GGWaveCodec? {
+        return when(specialVoice) {
+            SpecialVoice.SC -> makeCodec(0)
+            SpecialVoice.EU -> makeCodec(6)
+            null -> codecs[gender]?.get(age) ?: (makeCodec(9))
+        }
+
+
+    }
+    suspend fun speak(text: String,gender: Gender ,age : Age) {
+        val codec : GGWaveCodec? = codecFor(age,gender)
+        codec?.encodeAndPlay(text)
+    }
+}
