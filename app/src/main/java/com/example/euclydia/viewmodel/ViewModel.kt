6273a1c3497fbab90b0f08932c0d9709b7d64f30
kbb306 <kylerbbelshaw@gmail.com>
2026-03-11 08:47:18 -0600

    Added create() taking DNA object

--- app/src/main/java/com/example/euclydia/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/euclydia/viewmodel/ViewModel.kt
@@ -8,14 +8,19 @@ class EuclydiaViewModel : ViewModel() {
     private var shapeList : MutableList<Shape> = mutableListOf()
     private var microphone = Speech()
 
-    fun create(name : String, age: Age, gender: Gender, color : Int, sides : Int,
+    fun create(name : String, age: Age, gender: Gender, color : Int, sides : Int, // Raw data create(), may be deprecated soon?
               length : Double, x : Double, y : Double, heading : Double, speed :
                Double, lines : MutableList<String>) {
         val newShape  = Shape(name,age,gender,color,sides,length,x,y,heading,speed,lines)
         shapeList.add(newShape)
     }
 
-    fun create(dna : List<Any>) {
+    fun create(dna: DNA) { // used for import() and possibly standard shape creation
+        val newShape = Shape(dna)
+        shapeList.add(newShape)
+    }
+
+    fun create(dna : List<Any>) { // Used for  legacy import()
         val newShape  = Shape(dna)
         shapeList.add(newShape)
     }
