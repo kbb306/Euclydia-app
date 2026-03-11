bf36d3df3a5ed9eef2e5fd2dfa7dd3ae44810976
kbb306 <kylerbbelshaw@gmail.com>
2026-03-11 08:20:02 -0600

    Created create() methods taking in raw values and legacy lists, respectively.

--- app/src/main/java/com/example/euclydia/viewmodel/ViewModel.kt
+++ app/src/main/java/com/example/euclydia/viewmodel/ViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.euclydia.viewmodel
 
-class ViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.euclydia.model.*
+
+
+class EuclydiaViewModel : ViewModel() {
+    private var shapeList : MutableList<Shape> = mutableListOf()
+    private var microphone = Speech()
+
+    fun create(name : String, age: Age, gender: Gender, color : Int, sides : Int,
+               radius : Double, x : Double, y : Double, heading : Double, speed :
+               Double, lines : MutableList<String>) {
+        val newShape : Shape = Shape(name,age,gender,color,sides,radius,x,y,heading,speed,lines)
+        shapeList.add(newShape)
+    }
+
+    fun create(dna : List<Any>) {
+        val newShape : Shape = Shape(dna)
+        shapeList.add(newShape)
+    }
 }
