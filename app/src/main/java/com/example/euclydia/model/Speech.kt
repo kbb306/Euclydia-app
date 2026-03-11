2a45dee4678e6d24d4c84cd396ebd87d6080bc63
kbb306 <kylerbbelshaw@gmail.com>
2026-03-11 08:19:07 -0600

    Restructured maps and created companion method reverseBS to get usable data out of old python voice codes

--- app/src/main/java/com/example/euclydia/model/Speech.kt
+++ app/src/main/java/com/example/euclydia/model/Speech.kt
@@ -5,22 +5,55 @@ import br.com.chatnoir.ggwave_kotlin.GGWaveSampleFormat
 
 class Speech
  {
-    private val codecs = mapOf(
-        Gender.FEMALE to mapOf(
-            Age.CHILD to makeCodec(2),
-            Age.ADULT to makeCodec(1)
-        ),
-        Gender.MALE to mapOf(
-            Age.CHILD to makeCodec(8),
-            Age.ADULT to makeCodec(7)
-        ),
-        Gender.ANDROGYNOUS to mapOf(
-            Age.CHILD to makeCodec(10),
-            Age.ADULT to makeCodec(9)
-        )
-    )
+     companion object {
+         private val codecs = mapOf(
+             Gender.FEMALE to mapOf(
+                 Age.CHILD to 2,
+                 Age.ADULT to 1,
+             ),
+             Gender.MALE to mapOf(
+                 Age.CHILD to 8,
+                 Age.ADULT to 7,
+             ),
+             Gender.ANDROGYNOUS to mapOf(
+                 Age.CHILD to 10,
+                 Age.ADULT to 9
+             )
+         )
 
-        private fun makeCodec(protocol : Int) : GGWaveCodec {
+         val reverseLookup = codecs.flatMap { (gender, ages) ->
+             ages.map {(age,codec) ->
+                 codec to (gender to age)
+             }
+         }.toMap()
+
+         private val oldIDs = mapOf(
+             "SC" to 0,
+             "FA" to 1,
+             "FC" to 2,
+             "EU" to 6,
+             "MA" to 7,
+             "MC" to 8
+         )
+
+         fun reverseBS(code : String): VoiceRecord  {
+             return when(val protocol : Int? = oldIDs[code]) {
+                 0 -> VoiceRecord(Age.ADULT, Gender.FEMALE, SpecialVoice.SC)
+                 6 -> VoiceRecord(Age.ADULT, Gender.FEMALE, SpecialVoice.EU)
+                 else -> VoiceRecord((reverseLookup[protocol]!!).second,(reverseLookup[protocol]!!).first)
+             }
+         }
+
+     }
+
+     data class VoiceRecord(
+         val age: Age,
+         val gender: Gender,
+         val canon: SpecialVoice? = null
+     )
+
+
+     private fun makeCodec(protocol: Int) : GGWaveCodec {
         val voice = GGWaveCodec.Builder()
             .sampleRate(48000f)
             .sampleFormatInp(GGWaveSampleFormat.I16)
@@ -30,17 +63,17 @@ class Speech
             .build()
         return voice
     }
-    fun codecFor(age: Age, gender: Gender, specialVoice: SpecialVoice? = null): GGWaveCodec? {
+
+    fun codecFor(age: Age, gender: Gender, specialVoice: SpecialVoice? = null): GGWaveCodec {
         return when(specialVoice) {
             SpecialVoice.SC -> makeCodec(0)
             SpecialVoice.EU -> makeCodec(6)
-            null -> codecs[gender]?.get(age) ?: (makeCodec(9))
+            null -> makeCodec(codecs[gender]?.get(age) ?: 9)
         }
-
-
     }
+
     suspend fun speak(text: String,gender: Gender,age : Age, canon : SpecialVoice?) {
-        val codec : GGWaveCodec? = codecFor(age,gender,canon)
-        codec?.encodeAndPlay(text)
+        val codec : GGWaveCodec = codecFor(age,gender,canon)
+        codec.encodeAndPlay(text)
     }
 }
