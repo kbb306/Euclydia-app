a4bf6bda0f0cd4dc83916bc0101323a6f60bd670
kbb306 <kylerbbelshaw@gmail.com>
2026-03-13 13:39:35 -0600

    Revamped turn() to turnT0()

--- app/src/main/java/com/example/euclydia/model/Turtle.kt
+++ app/src/main/java/com/example/euclydia/model/Turtle.kt
@@ -2,16 +2,27 @@ package com.example.euclydia.model
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import kotlin.compareTo
 
 abstract class Turtle (
     val name : String,
     var x : Double,
     var y : Double,
-    var heading : Double,
+    heading : Double,
     var speed : Double,
     val color : Int,
 ) {
     var isFollowed : Boolean = false
+    var heading : Double = heading
+        set(value) {
+            field = normalize(value)
+        }
+
+    private fun normalize(angle: Double): Double {
+        var a = angle % 360.0
+        if (a < 0) a += 360.0
+        return a
+    }
 
     open fun update(worldHeight : Double ,worldWidth : Double) {
         forward(speed)
@@ -35,13 +46,23 @@ abstract class Turtle (
         this.y = y
     }
 
-    fun turn(degrees : Double) {
-        heading = when {
-            heading < 360 -> heading + degrees
-            else -> heading - degrees
-        }
+    private fun left(degrees: Double) {
+        heading += degrees
+    }
+    private fun right(degrees: Double) {
+        heading -= degrees
     }
 
+    fun turnTo(target : Double) {
+            val target = normalize(target)
+            val left = normalize(target  - heading)
+            val right = normalize(heading - target)
+            if (left <= right) {
+                left(left)
+            } else {
+                right(right)
+            }
+        }
     private fun wrap(worldWidth: Double, worldHeight: Double) {
         x = when {
             x < 0f.toDouble() -> x + worldWidth
@@ -56,10 +77,14 @@ abstract class Turtle (
         }
     }
 
-    fun collide() {
-        turn(180.00)
-        forward(60.00)
+    fun avoid(other : Turtle) : Double { // Calculate a safe heading
+        val dx = x - other.x
+        val dy = y - other.y
+        return Math.toDegrees(kotlin.math.atan2(dy,dx))
     }
 
     abstract fun draw(canvas: Canvas, paint: Paint, cameraX: Double, cameraY: Double)
 }
+
+
+
