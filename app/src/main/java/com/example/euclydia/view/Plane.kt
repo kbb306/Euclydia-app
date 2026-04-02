9b5bf0982cfa745e7fc952f0fe619be0bdc4b0be
kbb306 <kylerbbelshaw@gmail.com>
2026-04-02 12:42:05 -0600

    Created custom view

--- app/src/main/java/com/example/euclydia/view/Plane.kt
+++ app/src/main/java/com/example/euclydia/view/Plane.kt
@@ -1,6 +1,32 @@
 package com.example.euclydia.view
 
-class Plane @JvmOverloads constructor(
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.example.euclydia.model.Shape
 
-) {
+class Plane @JvmOverloads constructor(
+    context: Context,
+    attrs : AttributeSet? = null,
+) : View(context,attrs) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var shapes : List<Shape> = emptyList()
+
+    fun submit(newShapes: List<Shape>) {
+        shapes = newShapes
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.WHITE)
+
+        for (shape in shapes) {
+            shape.draw(canvas,paint,0.0,0.0)
+        }
+    }
 }
