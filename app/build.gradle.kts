4e6992be9694ac5921370b15370bf61556f0b29c
kbb306 <kylerbbelshaw@gmail.com>
2026-04-02 20:09:12 -0600

    Added ActivityViewModels support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -49,6 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("io.github.diogosq:ggwave-kotlin:1.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
