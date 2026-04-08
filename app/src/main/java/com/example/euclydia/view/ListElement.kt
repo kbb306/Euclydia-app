01bfe63c1da25b587bd678d2d4b2afde7d71703e
kbb306 <kylerbbelshaw@gmail.com>
2026-04-08 13:53:13 -0600

    Maybe the interactive elements should go here?

---
+++ app/src/main/java/com/example/euclydia/view/ListElement.kt
@@ -0,0 +1,11 @@
+package com.example.euclydia.view
+
+import android.app.Activity
+import android.os.Bundle
+
+class ListElement : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
