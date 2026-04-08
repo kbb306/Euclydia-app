539ce6c3dcbbd065aaea957eb67326242b215aca
kbb306 <kylerbbelshaw@gmail.com>
2026-04-08 13:52:44 -0600

    I hate recyclerviews

--- app/src/main/java/com/example/euclydia/viewmodel/ShapeAdaptor.kt
+++ app/src/main/java/com/example/euclydia/viewmodel/ShapeAdaptor.kt
@@ -1,24 +1,49 @@
 package com.example.euclydia.viewmodel
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.euclydia.databinding.ControlFragmentBinding
 import com.example.euclydia.databinding.ListActivityBinding
+import com.example.euclydia.databinding.ListElementBinding
+import com.example.euclydia.model.Shape
 import com.example.euclydia.view.ListActivity
 
-class ShapeAdaptor : RecyclerView.Adapter<ShapeListViewHolder>() {
+class ShapeAdaptor(private val shapelist : List<Shape>) : RecyclerView.Adapter<ShapeListViewHolder>() {
+    private var onClickListener : View.OnClickListener? = null
     override fun onCreateViewHolder(parent : ViewGroup, viewType: Int): ShapeListViewHolder {
-
-        return TODO("Provide the return value")
+        val binding = ListElementBinding.inflate(LayoutInflater.from(parent.context))
+        return ShapeListViewHolder(binding)
     }
 
     override fun onBindViewHolder(p0: ShapeListViewHolder, p1: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return shapelist.size
     }
+
+
 }
 
-class ShapeListViewHolder(val binding: ListActivityBinding) : RecyclerView.ViewHolder(binding.root) {}
+class ShapeListViewHolder(val binding: ListElementBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(shape : Shape) {
+        binding.apply {
+            shapeName.text = shape.name
+            checkBox.setOnCheckedChangeListener { button, bool ->
+                if (bool) {
+                    TODO("How to add to wider list of deletables in ListElement")
+                }
+
+                else {
+
+                }
+            }
+            shapeView.setImageDrawable(  TODO("Set icon to shape" ))
+
+        }
+    }
+
+}
