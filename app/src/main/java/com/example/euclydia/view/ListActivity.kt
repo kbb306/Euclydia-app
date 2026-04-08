791fe6deb92f7960385996dc664752a3a6c05036
kbb306 <kylerbbelshaw@gmail.com>
2026-04-08 14:27:21 -0600

    Added delist

--- app/src/main/java/com/example/euclydia/view/ListActivity.kt
+++ app/src/main/java/com/example/euclydia/view/ListActivity.kt
@@ -3,9 +3,11 @@ package com.example.euclydia.view
 import com.example.euclydia.viewmodel.EuclydiaViewModel
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.euclydia.model.Shape
 import kotlin.getValue
 
 
 class ListActivity : AppCompatActivity() {
     private val viewModel: EuclydiaViewModel by viewModels()
+    var delList : MutableList<Shape> = emptyList<Shape>() as MutableList<Shape> //Somehow add data from recyclerview here
 }
