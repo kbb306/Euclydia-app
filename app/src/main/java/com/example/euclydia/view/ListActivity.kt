e57d3deda772fde985a20d032098b248b5c0c493
kbb306 <kylerbbelshaw@gmail.com>
2026-04-08 19:33:38 -0600

    Added buttons

--- app/src/main/java/com/example/euclydia/view/ListActivity.kt
+++ app/src/main/java/com/example/euclydia/view/ListActivity.kt
@@ -1,13 +1,33 @@
 package com.example.euclydia.view
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.euclydia.viewmodel.EuclydiaViewModel
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.euclydia.databinding.ListActivityBinding
 import com.example.euclydia.model.Shape
 import kotlin.getValue
 
 
 class ListActivity : AppCompatActivity() {
+    private lateinit var binding: ListActivityBinding
     private val viewModel: EuclydiaViewModel by viewModels()
-    var delList : MutableList<Shape> = emptyList<Shape>() as MutableList<Shape> //Somehow add data from recyclerview here
+    var select_List : MutableList<Shape> = emptyList<Shape>() as MutableList<Shape> //Somehow add data from recyclerview here
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ListActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.back.setOnClickListener {
+            TODO("On stop call")
+        }
+
+        binding.selall.setOnClickListener {
+            TODO("Add all to list")
+        }
+
+        binding.delete.setOnClickListener {
+            TODO("Dialog fragment")
+        }
+    }
 }
