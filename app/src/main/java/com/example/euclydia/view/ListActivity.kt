b4337d7ba081c5b9e37952fc91c065fbd2397734
kbb306 <kylerbbelshaw@gmail.com>
2026-04-08 23:50:54 -0600

    Repaired ListActivity

--- app/src/main/java/com/example/euclydia/view/ListActivity.kt
+++ app/src/main/java/com/example/euclydia/view/ListActivity.kt
@@ -1,7 +1,6 @@
 package com.example.euclydia.view
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import com.example.euclydia.viewmodel.EuclydiaViewModel
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,19 +9,17 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.euclydia.databinding.ListActivityBinding
-import com.example.euclydia.model.Shape
 import com.example.euclydia.viewmodel.ShapeAdapter
 import kotlinx.coroutines.launch
-import java.util.UUID
 import kotlin.getValue
 
 
 class ListActivity : AppCompatActivity() {
     private lateinit var binding: ListActivityBinding
     private val viewModel: EuclydiaViewModel by viewModels()
-    private var backinglist = listOf<Shape>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val adapter = ShapeAdapter(
             emptyList(),
             viewModel.select_ids,
@@ -38,37 +35,48 @@ class ListActivity : AppCompatActivity() {
             }
         )
 
-        binding = ListActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val shapes =  viewModel.shapeList
+        binding.listView.layoutManager = LinearLayoutManager(this)
 
 
-        binding.listView.adapter = adapter
-        binding.listView.layoutManager = LinearLayoutManager(this
-        )
-        binding.back.setOnClickListener {
-            TODO("On stop call")
-        }
-
-        binding.selall.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
-                viewModel.select_ids.clear()
-                viewModel.select_ids.addAll(shapes.value.map { it.uuid })
-            } else {
-                viewModel.select_ids.clear()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.shapeList.collect { shapes ->
+                    adapter.updateShapes(shapes)
+                }
             }
-
         }
+                    binding = ListActivityBinding.inflate(layoutInflater)
+                    setContentView(binding.root)
 
-        binding.delete.setOnClickListener {
-            val delete = UniversalDialog(
-                title = "Confirm Deletion",
-                message = "Are you sure you want to delete the selected shapes?",
-                positive = "Yes",
-                negative = "No",
-                neutral = null
-            )
-        }
-    }
-}
+                    val shapes = viewModel.shapeList
+
+
+                    binding.listView.adapter = adapter
+                    binding.listView.layoutManager = LinearLayoutManager(
+                        this
+                    )
+                    binding.back.setOnClickListener {
+                        TODO("On stop call")
+                    }
+
+                    binding.selall.setOnCheckedChangeListener { _, isChecked ->
+                        if (isChecked) {
+                            viewModel.select_ids.clear()
+                            viewModel.select_ids.addAll(shapes.value.map { it.uuid })
+                        } else {
+                            viewModel.select_ids.clear()
+                        }
+
+                    }
+
+                    binding.delete.setOnClickListener {
+                        val delete = UniversalDialog(
+                            title = "Confirm Deletion",
+                            message = "Are you sure you want to delete the selected shapes?",
+                            positive = "Yes",
+                            negative = "No",
+                            neutral = null
+                        )
+                    }
+                }
+            }
