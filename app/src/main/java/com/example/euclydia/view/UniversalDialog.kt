d7818218ea981cef2e5344b954b15dcbaaa09279
kbb306 <kylerbbelshaw@gmail.com>
2026-04-12 17:46:52 -0600

    Changed buttons to match theme

--- app/src/main/java/com/example/euclydia/view/UniversalDialog.kt
+++ app/src/main/java/com/example/euclydia/view/UniversalDialog.kt
@@ -3,7 +3,10 @@ package com.example.euclydia.view
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import com.example.euclydia.R
+import com.google.android.material.button.MaterialButton
 
 class UniversalDialog(
     private val title: String,
@@ -38,5 +41,36 @@ class UniversalDialog(
         }
 
         return builder.create()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog as? AlertDialog
+        dialog?.let {
+            it.getButton(AlertDialog.BUTTON_POSITIVE)?.let {button ->
+                if(button is MaterialButton) {
+                    button.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.background))
+                    button.strokeWidth = (3 * resources.displayMetrics.density).toInt()
+                    button.setStrokeColorResource(R.color.blue)
+                    button.cornerRadius = (0 * resources.displayMetrics.density).toInt()
+                }
+            }
+            it.getButton(AlertDialog.BUTTON_NEUTRAL).let {button ->
+                if(button is MaterialButton) {
+                    button.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.background))
+                    button.strokeWidth = (3 * resources.displayMetrics.density).toInt()
+                    button.setStrokeColorResource(R.color.yellow)
+                    button.cornerRadius = (0 * resources.displayMetrics.density).toInt()
+                }
+            }
+            it.getButton(AlertDialog.BUTTON_NEGATIVE).let {button ->
+                if(button is MaterialButton) {
+                    button.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.background))
+                    button.strokeWidth = (3 * resources.displayMetrics.density).toInt()
+                    button.setStrokeColorResource(R.color.red)
+                    button.cornerRadius = (0 * resources.displayMetrics.density).toInt()
+                }
+            }
+        }
     }
 }
