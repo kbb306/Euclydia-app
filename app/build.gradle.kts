89acb5a7cebc0849d74cfadef8ff79caf5b1e460
kbb306 <kylerbbelshaw@gmail.com>
2026-04-13 16:50:52 -0600

    Changed minimum API to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.euclydia"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
