e33723389188057706156c5175ab9d86938b5944
kbb306 <kylerbbelshaw@gmail.com>
2026-04-13 19:15:27 -0600

    Wired up recyclerview in follow fragment

---
+++ app/src/main/java/com/example/euclydia/viewmodel/LineAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.euclydia.viewmodel
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.euclydia.databinding.LineEntryBinding
+import com.example.euclydia.model.Shape
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+
+class LineAdapter() : RecyclerView.Adapter<LineViewHolder>(){
+
+    private var lines : List<LineLogEntry> = emptyList()
+
+    fun submitLines(newLines : List<LineLogEntry>) {
+        lines = newLines
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(
+        p0: ViewGroup,
+        p1: Int
+    ): LineViewHolder {
+        val binding = LineEntryBinding.inflate(LayoutInflater.from(p0.context),p0,false)
+        return LineViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        p0: LineViewHolder,
+        p1: Int
+    ) {
+        p0.bind(lines[p1])
+    }
+
+    override fun getItemCount(): Int {
+        return lines.size
+    }
+
+}
+
+class LineViewHolder(val binding: LineEntryBinding,) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(linetext : LineLogEntry) {
+        binding.apply {
+            line.text = linetext.line
+        }
+    }
+
+}
